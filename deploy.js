c7c441d5b25763b2a3162f96434f520854e569db
kaysu97 <38857249+kaysu97@users.noreply.github.com>
2018-11-28 11:16:08 +0800

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,41 +1,47 @@
 const fs = require('fs')
 const Web3 = require('web3')
 
-
 let web3 = new Web3('http://localhost:8545')
 
 const abi = JSON.parse(fs.readFileSync('./contract/Bank_sol_Bank.abi').toString())
 const bytecode = '0x' + fs.readFileSync('./contract/Bank_sol_Bank.bin').toString()
 
-
+let bank = new web3.eth.Contract(abi)
 
 web3.eth.getAccounts().then(function (accounts) {
 
     // deploy contract
     
-	let bank =  new web3.eth.Contract(abi).deploy(
-   {
-     from: accounts[0], 
-     data: bytecode, 
-     gas: '4700000'
-   })
-  function (e, contract){
-   
-    console.log(e, contract);
-    if (typeof contract.address !== 'undefined') {
+    console.log("bank user: "+accounts[0]);
+    
 
-         console.log(' transactionHash: ' + contract.transactionHash + 
-         
-          'contractAddress: ' + contract.address 
-          
-          );
-    	fs.writeFileSync('address.txt', contract.address);
-    }
-
- }
-  .on('receipt', console.log)
-        .on('error', console.error)
-
+    bank.deploy({
+        data: bytecode,
+    })
+    .send({
+        from: accounts[0],
+        gas: 4700000
+       
+    })
+    .on('error', function(error){ 
+        console.log(error);
+    })
+    .on('transactionHash', function(transactionHash){
+      
+    })
+    .on('receipt', function(receipt){
+       console.log('receipt');
+       console.log(receipt);
+  
+    })
+    .on('confirmation', function(confirmationNumber, receipt){
+        console.log("confirmationNumber: "+confirmationNumber);
+        
+        
+    })
+    .then(function(newContractInstance){
+        console.log("Save newContractInstance.options.address: ");
+        console.log(newContractInstance.options.address);
+        fs.writeFileSync('./address.txt',newContractInstance.options.address)
+    });
 })
-
-
